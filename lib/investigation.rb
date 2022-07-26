2804244dc5f1cf616b57f7aa09d541b560e8a925
Jesper Olsson <software@jesperolsson.se>
2022-07-26 09:58:08 +0200

    #11 Investigation

---
+++ lib/investigation.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+##
+# Encapsulates information gathering about a pull request.
+class Investigation
+  ##
+  # Ctor.
+  #
+  # Expects the JSON to conform to the GitHub API.
+  # https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#pull_request
+  def initialize(json)
+    @json = json
+  end
+
+  ##
+  # Asks the object to provide a dossier for the pull request.
+  def dossier
+    format('%d files changed', @json['pull_request']['changed_files'])
+  end
+end
